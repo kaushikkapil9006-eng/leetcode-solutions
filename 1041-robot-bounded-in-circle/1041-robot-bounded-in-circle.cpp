f1e098673900031607801f2b99c42547a0b11e03
kaushikkapil9006-eng <kaushikkapil9006@gmail.com>
2026-05-12 08:33:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (73.05%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int x = 0, y = 0;
+        int dir = 0; // 0=N, 1=E, 2=S, 3=W
+
+        for(char ch : instructions) {
+            if(ch == 'G') {
+                if(dir == 0) y++;
+                else if(dir == 1) x++;
+                else if(dir == 2) y--;
+                else x--;
+            }
+            else if(ch == 'L') {
+                dir = (dir + 3) % 4;
+            }
+            else if(ch == 'R') {
+                dir = (dir + 1) % 4;
+            }
+        }
+
+        return (x == 0 && y == 0) || dir != 0;
+    }
+};
