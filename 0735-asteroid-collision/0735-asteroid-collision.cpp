34889d87c4e7f6c64d242a73f9791497b611a14f
KAPIL KAUSHIK <kaushikkapil9006@gmail.com>
2026-09-10 18:37:40 +0530

    Time: 3 ms (27.84%), Space: 21.6 MB (89.3%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> s;
+
+        for(int i = 0; i < asteroids.size(); i++) {
+
+            if(asteroids[i] > 0) {
+                s.push_back(asteroids[i]);
+            }
+            else {
+                while(!s.empty() && 
+                      s.back() > 0 && 
+                      s.back() < abs(asteroids[i])) {
+                    s.pop_back();
+                }
+
+                if(!s.empty() && s.back() == abs(asteroids[i])) {
+                    s.pop_back();
+                }
+                else if(s.empty() || s.back() < 0) {
+                    s.push_back(asteroids[i]);
+                }
+            }
+        }
+
+        return s;
+    }
+};
