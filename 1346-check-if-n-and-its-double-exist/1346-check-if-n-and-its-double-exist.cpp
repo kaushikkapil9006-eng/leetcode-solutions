a8793a2e6c5bc4bb30e4b9819e5be0e4a192bb7d
KAPIL KAUSHIK <kaushikkapil9006@gmail.com>
2026-09-22 17:26:09 +0530

    Time: 10 ms (5.9%), Space: 13.4 MB (85.96%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end());
+        for(int i = 0;i<arr.size();i++){
+            for(int j = 0;j<arr.size();j++){              
+                if( i!=j && arr[j]== 2*arr[i]){
+                    return true;
+                }  
+            }
+        }
+        return false;;
+        
+    }
+};
